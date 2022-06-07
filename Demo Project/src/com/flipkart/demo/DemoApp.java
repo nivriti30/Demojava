e0282c7a2072b7b0b7c93718bf90bdfb204affec
Nivriti Pandey <nivriti.pandey@BLRETV-42KXG8WN.local>
2022-06-07 08:46:56 +0530

    first commit

---
+++ Demo Project/src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,12 @@
+package com.flipkart.demo;
+
+/**
+ * @author nivriti.pandey
+ *
+ */
+public class DemoApp {
+    public static void main (String[] args)
+    {
+    	System.out.println("This is my first java app.") ;
+    }
+}
